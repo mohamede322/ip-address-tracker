6f9f15a0ed7681de36f332c412a0c64125370080
Psycho <93525954+itspsycholol322@users.noreply.github.com>
2022-04-19 06:34:35 +0200

    changed height

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="App" style={{ height: window.innerHeight }}>
       <Input
         arrow={arrow}
         value={IP}
